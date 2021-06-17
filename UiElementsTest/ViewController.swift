dc5b762dcaa185e4b2389afbcdb92b3bfe5fe6dd
J. Ivan Martinez Mateos <jimm0063@gmail.com>
2021-06-16 21:46:54 -0500

    Initial Commit

---
+++ UiElementsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiElementsTest
+//
+//  Created by J. Ivan Martinez Mateos on 16/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
